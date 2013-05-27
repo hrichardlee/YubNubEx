ff649ca069b25cf9da5e150e9ccb68c99adf8c34
Hyunho Richard Lee <hrichardlee@gmail.com>
2013-05-26 17:16:56 -0700

    removing build file

--- build.bat
+++
@@ -1,6 +0,0 @@
-set CHROME_INSTALL_LOCATION="%programfiles% (x86)\Google\Chrome\Application\chrome.exe"
-
-%CHROME_INSTALL_LOCATION% --pack-extension=%cd%\NoStone --pack-extension-key=%cd%\bin\nostone.pem --no-message-box
-
-del bin\NoStone.crx
-move NoStone.crx bin\NoStone.crx
